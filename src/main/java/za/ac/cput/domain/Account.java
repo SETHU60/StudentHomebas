9c44ec922dbe8c901f3c49968dc0136fa3368814
SETHU60 <promakhambeni@gmail.com>
2024-07-01 20:50:11 +0200

    Account and AccountFactory done

--- src/main/java/za/ac/cput/domain/Account.java
+++ src/main/java/za/ac/cput/domain/Account.java
@@ -1,33 +1,53 @@
 package za.ac.cput.domain;
 
+import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.util.Objects;
 
+@Entity
 public class Account {
-    private int AccountID;
-    private String AccountType;
-    private String PaymentInfo;
-    private double Balance;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int accountId;
+    private String accountType;
+    private String paymentInfo;
+    private double balance;
     private LocalDate paymentDueDate;
-    private  String paymentStatus;
-    private String AccountStatus;
+    private String paymentStatus;
+    private String accountStatus;
 
-    public Account(){}
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
-    public int getAccountID() {
-        return AccountID;
+    protected Account() {
+    }
+
+    private Account(AccountBuilder builder) {
+        this.accountId = builder.accountId;
+        this.accountType = builder.accountType;
+        this.paymentInfo = builder.paymentInfo;
+        this.balance = builder.balance;
+        this.paymentDueDate = builder.paymentDueDate;
+        this.paymentStatus = builder.paymentStatus;
+        this.accountStatus = builder.accountStatus;
+        this.user = builder.user;
+    }
+
+    public int getAccountId() {
+        return accountId;
     }
 
     public String getAccountType() {
-        return AccountType;
+        return accountType;
     }
 
     public String getPaymentInfo() {
-        return PaymentInfo;
+        return paymentInfo;
     }
 
     public double getBalance() {
-        return Balance;
+        return balance;
     }
 
     public LocalDate getPaymentDueDate() {
@@ -39,28 +59,99 @@ public class Account {
     }
 
     public String getAccountStatus() {
-        return AccountStatus;
+        return accountStatus;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return accountId == account.accountId &&
+                Double.compare(account.balance, balance) == 0 &&
+                Objects.equals(accountType, account.accountType) &&
+                Objects.equals(paymentInfo, account.paymentInfo) &&
+                Objects.equals(paymentDueDate, account.paymentDueDate) &&
+                Objects.equals(paymentStatus, account.paymentStatus) &&
+                Objects.equals(accountStatus, account.accountStatus) &&
+                Objects.equals(user, account.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountId, accountType, paymentInfo, balance, paymentDueDate, paymentStatus, accountStatus, user);
     }
 
     @Override
     public String toString() {
         return "Account{" +
-                "AccountID=" + AccountID +
-                ", AccountType='" + AccountType + '\'' +
-                ", PaymentInfo='" + PaymentInfo + '\'' +
-                ", Balance=" + Balance +
+                "accountId=" + accountId +
+                ", accountType='" + accountType + '\'' +
+                ", paymentInfo='" + paymentInfo + '\'' +
+                ", balance=" + balance +
                 ", paymentDueDate=" + paymentDueDate +
                 ", paymentStatus='" + paymentStatus + '\'' +
-                ", AccountStatus='" + AccountStatus + '\'' +
+                ", accountStatus='" + accountStatus + '\'' +
+                ", user=" + user +
                 '}';
     }
-    public boolean equals(Object o){
-        if (this == o) return true;
-        if(o == null || getClass() !=o.getClass()) return false;
-        Account account = (Account) o;
-        return AccountID == account.AccountID && Objects.equals(AccountID, account.AccountID) && Objects.equals(AccountType, account.AccountType) && Objects.equals(PaymentInfo, account.PaymentInfo) && Objects.equals(Balance, account.Balance) && Objects.equals(paymentDueDate, account.paymentDueDate) && Objects.equals(paymentStatus, account.paymentStatus) && Objects.equals(AccountStatus, account.AccountStatus);
-    }
-    public int hashCode(){
-        return Objects.hash(AccountID,AccountType,PaymentInfo,Balance,paymentDueDate,paymentStatus,AccountStatus);
+
+    public static class AccountBuilder {
+        private int accountId;
+        private String accountType;
+        private String paymentInfo;
+        private double balance;
+        private LocalDate paymentDueDate;
+        private String paymentStatus;
+        private String accountStatus;
+        private User user;
+
+        public AccountBuilder setAccountId(int accountId) {
+            this.accountId = accountId;
+            return this;
+        }
+
+        public AccountBuilder setAccountType(String accountType) {
+            this.accountType = accountType;
+            return this;
+        }
+
+        public AccountBuilder setPaymentInfo(String paymentInfo) {
+            this.paymentInfo = paymentInfo;
+            return this;
+        }
+
+        public AccountBuilder setBalance(double balance) {
+            this.balance = balance;
+            return this;
+        }
+
+        public AccountBuilder setPaymentDueDate(LocalDate paymentDueDate) {
+            this.paymentDueDate = paymentDueDate;
+            return this;
+        }
+
+        public AccountBuilder setPaymentStatus(String paymentStatus) {
+            this.paymentStatus = paymentStatus;
+            return this;
+        }
+
+        public AccountBuilder setAccountStatus(String accountStatus) {
+            this.accountStatus = accountStatus;
+            return this;
+        }
+
+        public AccountBuilder setUser(User user) {
+            this.user = user;
+            return this;
+        }
+
+        public Account build() {
+            return new Account(this);
+        }
     }
 }
---
+++ src/main/java/za/ac/cput/factory/AccountFactory.java
@@ -0,0 +1,23 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Account;
+import za.ac.cput.domain.User;
+
+import java.time.LocalDate;
+
+public class AccountFactory {
+
+    public static Account createAccount(int accountId, String accountType, String paymentInfo, double balance,
+                                        LocalDate paymentDueDate, String paymentStatus, String accountStatus, User user) {
+        return new Account.AccountBuilder()
+                .setAccountId(accountId)
+                .setAccountType(accountType)
+                .setPaymentInfo(paymentInfo)
+                .setBalance(balance)
+                .setPaymentDueDate(paymentDueDate)
+                .setPaymentStatus(paymentStatus)
+                .setAccountStatus(accountStatus)
+                .setUser(user)
+                .build();
+    }
+}
