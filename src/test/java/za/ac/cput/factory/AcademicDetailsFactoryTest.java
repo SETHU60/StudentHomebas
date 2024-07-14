b002afd6b78674309a5af0f267d89a337d87213a
Mlungisi <mmbuyazi@gmail.com>
2024-07-14 16:58:29 +0200

    factories for Property.java and AcademicDetails.java

--- src/test/java/za/ac/cput/factory/AcademicDetailsFactoryTest.java
+++ src/test/java/za/ac/cput/factory/AcademicDetailsFactoryTest.java
@@ -4,20 +4,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 import za.ac.cput.domain.AcademicDetails;
+import za.ac.cput.domain.Student;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AcademicDetailsFactoryTest {
     private AcademicDetails testAcademicDetails1;
     private AcademicDetails testAcademicDetails2;
-
+    private Student student1;
     @BeforeEach
     void setUp() {
-        testAcademicDetails1 = AcademicDetailsFactory.buildAcademicDetails("ACD01",
-                "221164014", "Cape Peninsula University of Technology", "Diploma in ICT", 3);
-
-        testAcademicDetails2 = AcademicDetailsFactory.buildAcademicDetails("ACD01",
-                null, "Cape Peninsula University of Technology", "Diploma in ICT", 3);
+        testAcademicDetails1 = AcademicDetailsFactory.buildAcademicDetails("ACD01", "Cape Peninsula University of Technology", "Diploma in ICT", 3);
+        testAcademicDetails2 = AcademicDetailsFactory.buildAcademicDetails("ACD01", "Cape Peninsula University of Technology", "Diploma in ICT", 0);
 
     }
 
@@ -29,7 +27,6 @@ class AcademicDetailsFactoryTest {
 
     @Test
     void buildAcademicDetailsWithNullValues() {
-    //Test should pass because object with any null value will return null
     assertNull(testAcademicDetails2);
 
     }
